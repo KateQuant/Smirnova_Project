425f72c0848e9b6aeed98ddac7eb1ae1c839ba37
KateQuant <ekaterina.smirnova@quantori.academy>
2022-12-04 00:37:48 +0100

    Create separate function plot_gc for unittesting

--- utils_old/misc/plot_for_test.py
+++ utils_old/misc/plot_for_test.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from pathlib import Path
 import re
 
 
@@ -13,35 +12,37 @@ def gc_content_plot(string: str, file_name, step=100):
     """Function that plots DNA sequence GC-content distribution"""
 
     dna_pattern = '^[ACTG]*$'
-    if re.findall(dna_pattern, string) is None and len(string) < step:
-        return f"Invalid string! It should contain only 'ATGC' letters and be longer than {step}"
 
-    gc_count = lambda dna_string: ((dna_string.count('G') + dna_string.count('C')) / len(dna_string)) * 100
+    if re.findall(dna_pattern, string) and len(string) > step:
 
-    partitions_list = [string[i: i + step] for i in range(0, len(string), step)]
-    partition_gc = list(map(gc_count, partitions_list))
-    genome_position = [i * step for i in range(1, len(partition_gc) + 1)]
+        gc_count = lambda dna_string: ((dna_string.count('G') + dna_string.count('C')) / len(dna_string)) * 100
 
-    gc_dataframe = pd.DataFrame({'GC content': partition_gc,
-                                 'Genome position': genome_position})
+        partitions_list = [string[i: i + step] for i in range(0, len(string), step)]
+        partition_gc = list(map(gc_count, partitions_list))
+        genome_position = [i * step for i in range(1, len(partition_gc) + 1)]
 
-    x_ticks = None
-    xtick_labels = None
-    if len(genome_position) > 20:
-        x_ticks, xtick_labels = ticks_mapper(genome_position, gc_dataframe)
+        gc_dataframe = pd.DataFrame({'GC content': partition_gc,
+                                     'Genome position': genome_position})
 
-    plt.figure(figsize=(15, 8))
-    ax = sns.barplot(x='Genome position', y='GC content', data=gc_dataframe, color=(0.7, 0.3, 0.2))
-    ax.axes.set_title('GC content distribution', fontsize=20)
-    ax.set_xlabel("Genome position", fontsize=20)
-    ax.set_ylabel("GC content", fontsize=20)
-    ax.tick_params(labelsize=10)
-    if x_ticks:
-        ax.set(xticks=x_ticks)
-        ax.set(xticklabels=xtick_labels)
+        x_ticks = None
+        xtick_labels = None
+        if len(genome_position) > 20:
+            x_ticks, xtick_labels = ticks_mapper(genome_position, gc_dataframe)
 
-    picture = f"{PATH_TO_DOCS_FROM_NCBI}/{file_name}.png"
-    plt.savefig(picture)
-    path_to_picture = Path().joinpath(picture)
+        plt.figure(figsize=(15, 8))
+        ax = sns.barplot(x='Genome position', y='GC content', data=gc_dataframe, color=(0.7, 0.3, 0.2))
+        ax.axes.set_title('GC content distribution', fontsize=20)
+        ax.set_xlabel("Genome position", fontsize=20)
+        ax.set_ylabel("GC content", fontsize=20)
+        ax.tick_params(labelsize=10)
+        if x_ticks:
+            ax.set(xticks=x_ticks)
+            ax.set(xticklabels=xtick_labels)
 
-    return path_to_picture
+        picture = f"{PATH_TO_DOCS_FROM_NCBI}{file_name}.png"
+        plt.savefig(picture)
+
+        return picture
+
+    return f"Invalid string! It should contain only 'ATGC' letters and be longer than {step}"
+
